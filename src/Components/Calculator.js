2c8d5231419b4b55ff0a8a0462b86f06de5bff11
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-03-14 16:50:00 +0200

    Add calculator structure feature

---
+++ src/Components/Calculator.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import CalcButton from './CalculatorBtn';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calc-body">
+        <div className="calc-screen">
+          0
+        </div>
+        <div className="keyboard">
+          <div className="key-digits">
+            <CalcButton text="AC" />
+            <CalcButton text="+/-" />
+            <CalcButton text="%" />
+            <CalcButton text="7" />
+            <CalcButton text="8" />
+            <CalcButton text="9" />
+            <CalcButton text="4" />
+            <CalcButton text="5" />
+            <CalcButton text="6" />
+            <CalcButton text="1" />
+            <CalcButton text="2" />
+            <CalcButton text="3" />
+            <CalcButton text="0" />
+            <div className="key-dot">
+              <CalcButton text="." />
+            </div>
+          </div>
+          <div className="operators">
+            <CalcButton text="÷" />
+            <CalcButton text="x" />
+            <CalcButton text="-" />
+            <CalcButton text="+" />
+            <CalcButton text="=" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
