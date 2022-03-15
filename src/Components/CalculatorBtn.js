5202f45e66a4590d4347d574f821ac98e1e2b874
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-03-15 08:51:53 +0200

    Add events to the calculator

--- src/Components/CalculatorBtn.js
+++ src/Components/CalculatorBtn.js
@@ -5,18 +5,26 @@ class CalcButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { text, onClick, mathObj } = this.props;
+    onClick(mathObj, text);
   }
 
   render() {
     const { text } = this.props;
     return (
-      <button type="button" className="calc-buttons">{text}</button>
+      <button type="button" className="calc-buttons" onClick={this.handleClick}>{text}</button>
     );
   }
 }
 
 CalcButton.propTypes = {
   text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  mathObj: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default CalcButton;
