62490ff48c805bd2b3b9955d4b07fce8d1f6bed4
Awais Amjed <awaisamjed171@gmail.com>
2022-03-22 13:25:19 +0500

    Update Calculator Button Test

--- src/test/component_tests/CalcButton.test.js
+++ src/test/component_tests/CalcButton.test.js
@@ -1,19 +1,19 @@
-import renderer from 'react-test-renderer';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import CalcButton from '../../Components/CalculatorBtn';
 import calculate from '../../Logic.js/Calculate';
 
 describe('Testing Calculator Button', () => {
   test('Click a Button on Calculator', () => {
     const calcObj = {
-      total: 0,
+      total: null,
       next: null,
       operation: null,
     };
 
-    const tree = renderer
-      .create(<CalcButton text="9" onClick={() => calculate(calcObj, '9')} mathObj={calcObj} />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+    render(
+      <CalcButton text="9" onClick={() => calculate(calcObj, '9')} mathObj={calcObj} />,
+    );
+    screen.getByText('9');
   });
 });
